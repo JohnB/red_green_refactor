cc50a45c07fd7b04cd5deaf14b47edf355c31151
John Baylor <john@anyperk.com>
2015-11-11 18:01:25 -0800

    Fixed spec to use a .square() method to get the array.

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -11,7 +11,7 @@ describe Square do
         32223
         33333
       )
-      expect(Square.new).to eq(expected_result)
+      expect(Square.new.square).to eq(expected_result)
     end
   end
 
@@ -25,7 +25,7 @@ describe Square do
         212
         222
       )
-      expect(Square.new(size: 2)).to eq(expected_result)
+      expect(Square.new(size: 2).square).to eq(expected_result)
     end
   end
 
@@ -34,13 +34,13 @@ describe Square do
       expected_result = %w(
         4444444
         4333334
-        43   34
-        43   34
-        43   34
+        43...34
+        43...34
+        43...34
         4333334
         4444444
       )
-      expect(Square.new(size: 4, limit: 2)).to eq(expected_result)
+      expect(Square.new(size: 4, limit: 2).square).to eq(expected_result)
     end
   end
 
@@ -53,7 +53,7 @@ describe Square do
         CBBBC
         CCCCC
       )
-      expect(Square.new(start: 'a')).to eq(expected_result)
+      expect(Square.new(start: 'a').square).to eq(expected_result)
     end
   end
 end
